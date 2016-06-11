2a8f98bb5a6b0f1212e555a37ccd2d4afab88602
Alberto Lusiani <alusiani@gmail.com>
2016-06-11 20:38:05 +0200

    recent modifications by Antonio Gioiosa added

--- NapoliDaq/analyzer/NaDaqAna.cc
+++ NapoliDaq/analyzer/NaDaqAna.cc
@@ -9,11 +9,11 @@
 void NaDaqAna::Loop(string const &filename) {
   cout << "Loop()" << endl;
   if (fChain == 0) return;
-  
+
   // nentries = fChain->GetEntriesFast();
   nentries = fChain->GetEntries();
   cout << "files contain " << nentries << " entries" << endl;
-  
+
   initialize(filename);
 
   Int_t nbytes = 0, nb = 0;
@@ -29,12 +29,14 @@ void NaDaqAna::Loop(string const &filename) {
 }
 
 void NaDaqAna::initialize(string const &filename) {
-  iter = 0;
+  pmt_iter = 0;
+  p1_iter = 0;
+  p2_iter = 0;
 
   pmt_Fired = new Double_t[nentries];
   p1_Fired = new Double_t[nentries];
   p2_Fired = new Double_t[nentries];
-	
+
   pmt_adcval = new Double_t[nentries];
   p1_adcval = new Double_t[nentries];
   p2_adcval = new Double_t[nentries];
@@ -72,96 +74,89 @@ void NaDaqAna::initialize(string const &filename) {
 
 void NaDaqAna::execute() {
 
-  pmt_Fired[iter] = pmt_fired;
-  p1_Fired[iter] = pin1_fired;
-  p2_Fired[iter] = pin2_fired;
-  
-  pmt_adcval[iter] = pmt_ADCVal*1.;
-  p1_adcval[iter] = pin1_ADCVal*1.;
-  p2_adcval[iter] = pin2_ADCVal*1.;
+  if (pmt_fired==1) {
+    pmt_Fired[pmt_iter] = pmt_fired;
+    pmt_adcval[pmt_iter] = pmt_ADCVal*1.;
+    pmt_nbof[pmt_iter] = pmt_NBOF*1.;
+    pmt_ntimetrgbof[pmt_iter] = pmt_NTimeTrgBOF*1.;
+    pmt_vbias[pmt_iter] = pmt_Vbias;//0.437*
+    pmt_extemp[pmt_iter] = pmt_extTemp*200./2047.-50.;
+    pmt_boardtemp[pmt_iter] = pmt_boardTemp*200./2047.-50.;
+    pmt_csptemp[pmt_iter] = pmt_cspTemp*1.;
+    pmt_time[pmt_iter] = (pmt_t_day*86400. + pmt_t_secday)/3600.-72.;
+    pmt_iter++;
+  }
 
-  pmt_nbof[iter] = pmt_NBOF*1.;
-  p1_nbof[iter] = pin1_NBOF*1.;
-  p2_nbof[iter] = pin1_NBOF*1.;
+  if (pin1_fired==1) {
+    p1_Fired[p1_iter] = pin1_fired;
+    p1_adcval[p1_iter] = pin1_ADCVal*1.;
+    p1_nbof[p1_iter] = pin1_NBOF*1.;
+    p1_ntimetrgbof[p1_iter] = pin2_NTimeTrgBOF*1.;
+    p1_vbias[p1_iter] = pin1_Vbias;
+    p1_extemp[p1_iter] = pin1_extTemp*200./2047.-50.;
+    p1_boardtemp[p1_iter] = pin1_boardTemp*200./2047.-50.;
+    p1_csptemp[p1_iter] = pin1_cspTemp*1.;
+    p1_time[p1_iter] = (pin1_t_day*86400. + pin1_t_secday)/3600. -72.;
+    p1_iter++;
+  }
 
-  pmt_ntimetrgbof[iter] = pmt_NTimeTrgBOF*1.;
-  p2_ntimetrgbof[iter] = pin1_NTimeTrgBOF*1.;
-  p1_ntimetrgbof[iter] = pin2_NTimeTrgBOF*1.;
+  if (pin2_fired==1) {
+    p2_Fired[p2_iter] = pin2_fired;
+    p2_adcval[p2_iter] = pin2_ADCVal*1.;
+    p2_nbof[p2_iter] = pin1_NBOF*1.;
+    p2_ntimetrgbof[p2_iter] = pin1_NTimeTrgBOF*1.;
+    p2_vbias[p2_iter] = pin2_Vbias;
+    p2_extemp[p2_iter] = pin2_extTemp*200./2047.-50.;
+    p2_boardtemp[p2_iter] = pin2_boardTemp*200./2047.-50.;
+    p2_csptemp[p2_iter] = pin2_cspTemp*1.;
+    p2_time[p2_iter] = (pin2_t_day*86400. + pin2_t_secday)/3600. -72.;
+    p2_iter++;
+  }
 
-  pmt_vbias[iter] = pmt_Vbias; //0.437*
-  p1_vbias[iter] = pin1_Vbias;
-  p2_vbias[iter] = pin2_Vbias;
-
-  pmt_extemp[iter] = pmt_extTemp*200./2047.-50.;
-  p1_extemp[iter] = pin1_extTemp*200./2047.-50.;
-  p2_extemp[iter] = pin2_extTemp*200./2047.-50.;
-
-  pmt_boardtemp[iter] = pmt_boardTemp*200./2047.-50.;
-  p1_boardtemp[iter] = pin1_boardTemp*200./2047.-50.;
-  p2_boardtemp[iter] = pin2_boardTemp*200./2047.-50.;
-
-  pmt_csptemp[iter] = pmt_cspTemp*1.;
-  p1_csptemp[iter] = pin1_cspTemp*1.;
-  p2_csptemp[iter] = pin2_cspTemp*1.;
-
-  pmt_time[iter] = (pmt_t_day*86400. + pmt_t_secday)/3600.-72.;
-  p1_time[iter] = (pin1_t_day*86400. + pin1_t_secday)/3600. -72.;
-  p2_time[iter] = (pin2_t_day*86400. + pin2_t_secday)/3600. -72.;
-
-  // pmt_time[iter] = (pmt_t_day*86400. + pmt_t_secday)/3600.-pmt_t_day*24.;
-  // p1_time[iter] = (pin1_t_day*86400. + pin1_t_secday)/3600.-pin1_t_day*24.;
-  // p2_time[iter] = (pin2_t_day*86400. + pin2_t_secday)/3600.-pin2_t_day*24.;
-
-  // pmt_time[iter] = pmt_t_secday;
-  // p1_time[iter] =  pin1_t_secday;
-  // p2_time[iter] =  pin2_t_secday;
-	
-  iter++;
-
-  //cout << "execute()" << endl;
+  // cout << "execute()" << endl;
 }
 
 void NaDaqAna::finalize(string const &filename) {
 
   //histograms for LM analysis
-  Double_t max_pmt_time = TMath::MaxElement(iter, pmt_time) +1;
-  Double_t max_p1_time = TMath::MaxElement(iter, p1_time) +1;
-  Double_t max_p2_time = TMath::MaxElement(iter, p2_time) +1;
+  Double_t max_pmt_time = TMath::MaxElement(pmt_iter, pmt_time) +1;
+  Double_t max_p1_time = TMath::MaxElement(p1_iter, p1_time) +1;
+  Double_t max_p2_time = TMath::MaxElement(p2_iter, p2_time) +1;
 
-  //--- n. of hours in 3-19 June
+  //--- n. of hours in period 3-15 June
   max_pmt_time = 24*(15-3+1);
   max_p1_time = 24*(15-3+1);
-  max_p2_time = 24*(15-3+1);  
+  max_p2_time = 24*(15-3+1);
 
-  Double_t nbof_max_pmt = TMath::MaxElement(iter, pmt_nbof);
-  Double_t nbof_max_p1 = TMath::MaxElement(iter, p1_nbof);
-  Double_t nbof_max_p2 = TMath::MaxElement(iter, p2_nbof);
+  Double_t nbof_max_pmt = TMath::MaxElement(pmt_iter, pmt_nbof);
+  Double_t nbof_max_p1 = TMath::MaxElement(p1_iter, p1_nbof);
+  Double_t nbof_max_p2 = TMath::MaxElement(p2_iter, p2_nbof);
 
-  Double_t ntimetrgbof_max_pmt = TMath::MaxElement(iter, pmt_ntimetrgbof);
-  Double_t ntimetrgbof_max_p1 = TMath::MaxElement(iter, p1_ntimetrgbof);
-  Double_t ntimetrgbof_max_p2 = TMath::MaxElement(iter, p2_ntimetrgbof);
+  Double_t ntimetrgbof_max_pmt = TMath::MaxElement(pmt_iter, pmt_ntimetrgbof);
+  Double_t ntimetrgbof_max_p1 = TMath::MaxElement(p1_iter, p1_ntimetrgbof);
+  Double_t ntimetrgbof_max_p2 = TMath::MaxElement(p2_iter, p2_ntimetrgbof);
 
-  Double_t pmt_vbias_mean = TMath::Mean(iter, pmt_vbias);
-  Double_t p1_vbias_mean = TMath::Mean(iter, p1_vbias);
-  Double_t p2_vbias_mean = TMath::Mean(iter, p2_vbias);
+  Double_t pmt_vbias_mean = TMath::Mean(pmt_iter, pmt_vbias);
+  Double_t p1_vbias_mean = TMath::Mean(p1_iter, p1_vbias);
+  Double_t p2_vbias_mean = TMath::Mean(p2_iter, p2_vbias);
 
   Int_t nbins=2;
-  
+
   TString title;
 
   title = "Laser PMT value vs. time";
   prof_pmt_ls_adcval = new TProfile("prof_pmt_laser_adcval", title, max_pmt_time*4, 0, max_pmt_time);
   prof_pmt_ls_adcval->SetStats(0);
-  
+
   title = "Am PMT value vs. time";
   prof_pmt_am_adcval = new TProfile("prof_pmt_am_adcval", title, max_pmt_time*4, 0, max_pmt_time);
   prof_pmt_am_adcval->SetStats(0);
-  
+
   title = "Laser PMT peak time vs. time";
   h2_pmt_nbof = new TH2D("h2_pmt_nbof", title, 1000, 0, max_pmt_time,  1000, 0, nbof_max_pmt);
-  
-  // title = "Histogram for pmt NTimeTrgBOF Value";
-  // h1_pmt_ntimetrgbof = new TH1D("h1_pmt_ntimetrgbof", title, 1000, 0, ntimetrgbof_max_pmt);
+
+  title = "Histogram for pmt NTimeTrgBOF Value";
+  h1_pmt_ntimetrgbof = new TH1D("h1_pmt_ntimetrgbof", title, 1000, 0, ntimetrgbof_max_pmt);
 
   title = "Laser PMT NTimeTrgBOF vs. time scatter plot";
   h2_pmt_ntimetrgbof = new TH2D("h2_pmt_ntimetrgbof", title, 2000, 0, max_pmt_time,  1000, 0, ntimetrgbof_max_pmt);
@@ -169,19 +164,19 @@ void NaDaqAna::finalize(string const &filename) {
   title = "PMT V_bias vs. time";
   prof_pmt_vbias = new TProfile("prof_pmt_vbias", title, max_pmt_time/nbins, 0, max_pmt_time);
   prof_pmt_vbias->SetStats(0);
-  
+
   title = "PMT external temperature vs. time";
   prof_pmt_extemp = new TProfile("prof_pmt_extemp", title, max_pmt_time/nbins, 0, max_pmt_time);
   prof_pmt_extemp->SetStats(0);
-  
+
   title = "PMT board temperature vs. time";
   prof_pmt_boardtemp = new TProfile("prof_pmt_boardtemp", title, max_pmt_time/nbins, 0, max_pmt_time);
   prof_pmt_boardtemp->SetStats(0);
-  
+
   title = "PMT preamp temperature vs. time";
   prof_pmt_csptemp = new TProfile("prof_pmt_csptemp", title, max_pmt_time/nbins, 0, max_pmt_time);
   prof_pmt_csptemp->SetStats(0);
-  
+
   title = "Histogram for pmt ADC Value";
   h_pmt_adcval = new TH1D("h_pmt_adcval", title, 200, 0, 10000);
 
@@ -202,19 +197,19 @@ void NaDaqAna::finalize(string const &filename) {
   title = "Time profile for pin1 Vbias Value";
   prof_pin1_vbias = new TProfile("prof_pin1_vbias", title, max_p1_time/nbins, 0, max_p1_time);
   prof_pin1_vbias->SetStats(0);
-  
+
   title = "Time profile for pin1 external Temperature Value";
   prof_pin1_extemp = new TProfile("prof_pin1_extemp", title, max_p1_time/nbins, 0, max_p1_time);
   prof_pin1_extemp->SetStats(0);
-  
+
   title = "Time profile for pin1 board Temperature Value";
   prof_pin1_boardtemp = new TProfile("prof_pin1_boardtemp", title, max_p1_time/nbins, 0, max_p1_time);
   prof_pin1_boardtemp->SetStats(0);
-  
+
   title = "Time profile for pin1 csp Temperature Value";
   prof_pin1_csptemp = new TProfile("prof_pin1_csptemp", title, max_p1_time/nbins, 0, max_p1_time);
   prof_pin1_csptemp->SetStats(0);
-  
+
   title = "Histogram for pin1 ADC Value";
   h_pin1_adcval = new TH1D("h_pin1_adcval", title, 1000, 0, 2000);
 
@@ -226,29 +221,29 @@ void NaDaqAna::finalize(string const &filename) {
   title = "Time profile for pin2 NBOF Value";
   h2_pin2_nbof = new TH2D("h2_pin2_nbof", title, 1000, 0, max_p2_time, 1000, 0, nbof_max_p2);
   h2_pin2_nbof->SetStats(0);
-  
+
   title = "Histogram for pin2 NTimeTrgBOF Value";
   h1_pin2_ntimetrgbof = new TH1D("h1_pin2_ntimetrgbof", title, 1000, 0, ntimetrgbof_max_p2);
 
   title = "Time profile for pin2 NTimeTrgBOF Value";
   h2_pin2_ntimetrgbof = new TH2D("h2_pin2_ntimetrgbof", title, 2000, 0, max_p2_time, 1000, 0, ntimetrgbof_max_p2);
   h2_pin2_ntimetrgbof->SetStats(0);
-  
+
   title = "Time profile for pin2 Vbias Value";
   prof_pin2_vbias = new TProfile("prof_pin2_vbias", title, max_p2_time/nbins, 0, max_p2_time);
 
   title = "Time profile for pin2 external Temperature Value";
   prof_pin2_extemp = new TProfile("prof_pin2_extemp", title, max_p2_time/nbins, 0, max_p2_time);
   prof_pin2_extemp->SetStats(0);
-  
+
   title = "Time profile for pin2 board Temperature Value";
   prof_pin2_boardtemp = new TProfile("prof_pin2_boardtemp", title, max_p2_time/nbins, 0, max_p2_time);
   prof_pin2_boardtemp->SetStats(0);
-  
+
   title = "Time profile for pin2 csp Temperature Value";
   prof_pin2_csptemp = new TProfile("prof_pin2_csptemp", title, max_p2_time/nbins, 0, max_p2_time);
   prof_pin2_csptemp->SetStats(0);
-  
+
   title = "Time profile for pin ratio ADC Value";
   prof_pin1Dpin2_adcval = new TProfile("prof_pin1Dpin2_adcval", title, max_p2_time/nbins, 0, max_p2_time);
   prof_pin1Dpin2_adcval->SetStats(0);
@@ -260,68 +255,66 @@ void NaDaqAna::finalize(string const &filename) {
 
   title = "Pin1 Vs Pin2 Correlation ADC Value";
   h2_pin1Vpin2_adcval = new TH2D("h2_pin1Vpin2_adcval", title, 1000, 0, 2000, 1000, 0, 2000);
-  
+
   title = "Histogram for pin2 ADC Value";
   h_pin2_adcval = new TH1D("h_pin2_adcval", title, 1000, 0, 2000);
-  
-  for (int j = 0; j < iter; ++j) {
-    
-    //if (pmt_time[j] > 7 && pmt_time[j] < 14 && p1_time[j] > 7 && p1_time[j] <14 && p2_time[j] > 7 && p2_time[j] <14) {
-    if (pmt_adcval[j]>5300 && pmt_adcval[j]<10000) prof_pmt_ls_adcval->Fill(pmt_time[j], pmt_adcval[j]);
-    if (pmt_adcval[j]>3000 && pmt_adcval[j]<5300) prof_pmt_am_adcval->Fill(pmt_time[j], pmt_adcval[j]);
-    if (p1_Fired[j]>0&&p1_adcval[j]>920&&p1_adcval[j]<1020) prof_pin1_adcval->Fill(p1_time[j], p1_adcval[j]);
-    if (p2_Fired[j]>0&&p2_adcval[j]>700&&p2_adcval[j]<780) prof_pin2_adcval->Fill(p2_time[j], p2_adcval[j]);
-    //if (p2_adcval[j]>0&&p1_adcval[j]>0) prof_pin1Dpin2_adcval->Fill(p2_time[j], p1_adcval[j]/p2_adcval[j]);
 
-    if (pmt_Fired[j]>0) h2_pmt_nbof->Fill(pmt_time[j], pmt_nbof[j]);
-    if (p1_Fired[j]>0&&p1_adcval[j]>920&&p1_adcval[j]<1020) h2_pin1_nbof->Fill(p1_time[j], p1_nbof[j]);
-    if (p2_Fired[j]>0&&p2_adcval[j]>700&&p2_adcval[j]<780) h2_pin2_nbof->Fill(p2_time[j], p2_nbof[j]);
-    //h2_pin1Vpin2_adcval->Fill(p1_adcval[j], p2_adcval[j]);
+  for (int j = 0; j < pmt_iter; ++j) {
+    if (pmt_adcval[j]>5300 && pmt_adcval[j]<10000)prof_pmt_ls_adcval->Fill(pmt_time[j],pmt_adcval[j]);
+    if (pmt_adcval[j]>3000 && pmt_adcval[j]<5300)prof_pmt_am_adcval->Fill(pmt_time[j],pmt_adcval[j]);
+    h2_pmt_nbof->Fill(pmt_time[j],pmt_nbof[j]);
+    h1_pmt_ntimetrgbof->Fill(pmt_ntimetrgbof[j]/10);
+    h2_pmt_ntimetrgbof->Fill(pmt_time[j],pmt_ntimetrgbof[j]);
+    prof_pmt_extemp->Fill(pmt_time[j],pmt_extemp[j]);
+    prof_pmt_boardtemp->Fill(pmt_time[j],pmt_boardtemp[j]);
+    prof_pmt_csptemp->Fill(pmt_time[j],pmt_csptemp[j]);
+    prof_pmt_vbias->Fill(pmt_time[j],pmt_vbias[j]*0.437/pmt_vbias_mean);
+    h_pmt_adcval->Fill(pmt_adcval[j]);
+  }
 
-    // if (pmt_Fired[j]>0) h1_pmt_ntimetrgbof->Fill(pmt_ntimetrgbof[j]/10);
-    if (p1_Fired[j]>0&&p1_adcval[j]>920&&p1_adcval[j]<1020) h1_pin1_ntimetrgbof->Fill(p1_ntimetrgbof[j]/10);
-    if (p2_Fired[j]>0&&p2_adcval[j]>700&&p2_adcval[j]<780) h1_pin2_ntimetrgbof->Fill(p2_ntimetrgbof[j]/10);
+  for (int j = 0; j < p1_iter; ++j){
+    if(p1_adcval[j]>920 && p1_adcval[j]<1020){
+      prof_pin1_adcval->Fill(p1_time[j],p1_adcval[j]);
+      h2_pin1_nbof->Fill(p1_time[j],p1_nbof[j]);
+      h1_pin1_ntimetrgbof->Fill(p1_ntimetrgbof[j]/10);
+      h2_pin1_ntimetrgbof->Fill(p1_time[j],p1_ntimetrgbof[j]);
+      prof_pin1_extemp->Fill(p1_time[j],p1_extemp[j]);
+      prof_pin1_boardtemp->Fill(p1_time[j],p1_boardtemp[j]);
+      prof_pin1_csptemp->Fill(p1_time[j],p1_csptemp[j]);
+      prof_pin1_vbias->Fill(p1_time[j],p1_vbias[j]*(-30.)/p1_vbias_mean);
+      h_pin1_adcval->Fill(p1_adcval[j]);
+    }
+  }
 
-    if (pmt_Fired[j]>0) h2_pmt_ntimetrgbof->Fill(pmt_time[j], pmt_ntimetrgbof[j]);
-    if (p1_Fired[j]>0&&p1_adcval[j]>920&&p1_adcval[j]<1020) h2_pin1_ntimetrgbof->Fill(p1_time[j], p1_ntimetrgbof[j]);
-    if (p2_Fired[j]>0&&p2_adcval[j]>700&&p2_adcval[j]<780) h2_pin2_ntimetrgbof->Fill(p2_time[j], p2_ntimetrgbof[j]);
-
-    if (pmt_Fired[j]>0) prof_pmt_extemp->Fill(pmt_time[j], pmt_extemp[j]);
-    if (p1_Fired[j]>0&&p1_adcval[j]>920&&p1_adcval[j]<1020) prof_pin1_extemp->Fill(p1_time[j], p1_extemp[j]);
-    if (p2_Fired[j]>0&&p2_adcval[j]>700&&p2_adcval[j]<780) prof_pin2_extemp->Fill(p2_time[j], p2_extemp[j]);
-
-    if (pmt_Fired[j]>0) prof_pmt_boardtemp->Fill(pmt_time[j], pmt_boardtemp[j]);
-    if (p1_Fired[j]>0&&p1_adcval[j]>920&&p1_adcval[j]<1020) prof_pin1_boardtemp->Fill(p1_time[j], p1_boardtemp[j]);
-    if (p2_Fired[j]>0&&p2_adcval[j]>700&&p2_adcval[j]<780) prof_pin2_boardtemp->Fill(p2_time[j], p2_boardtemp[j]);
-
-    if (pmt_Fired[j]>0) prof_pmt_csptemp->Fill(pmt_time[j], pmt_csptemp[j]);
-    if (p1_Fired[j]>0&&p1_adcval[j]>920&&p1_adcval[j]<1020) prof_pin1_csptemp->Fill(p1_time[j], p1_csptemp[j]);
-    if (p2_Fired[j]>0&&p2_adcval[j]>700&&p2_adcval[j]<780) prof_pin2_csptemp->Fill(p2_time[j], p2_csptemp[j]);
-
-    if (pmt_Fired[j]>0) prof_pmt_vbias->Fill(pmt_time[j], pmt_vbias[j]*0.437/pmt_vbias_mean);
-    if (p1_Fired[j]>0&&p1_adcval[j]>920&&p1_adcval[j]<1020) prof_pin1_vbias->Fill(p1_time[j], p1_vbias[j]*(-30.)/p1_vbias_mean);
-    if (p2_Fired[j]>0&&p2_adcval[j]>700&&p2_adcval[j]<780) prof_pin2_vbias->Fill(p2_time[j], p2_vbias[j]*(-30.)/p2_vbias_mean);
-
-    if (pmt_Fired[j]>0) h_pmt_adcval->Fill(pmt_adcval[j]);
-    if (p1_Fired[j]>0&&p1_adcval[j]>920&&p1_adcval[j]<1020) h_pin1_adcval->Fill(p1_adcval[j]);
-    if (p2_Fired[j]>0&&p2_adcval[j]>700&&p2_adcval[j]<780) h_pin2_adcval->Fill(p2_adcval[j]);
+  for (int j = 0; j < p2_iter; ++j){
+    if(p2_adcval[j]>700 && p2_adcval[j]<780){
+      prof_pin2_adcval->Fill(p2_time[j],p2_adcval[j]);
+      h2_pin2_nbof->Fill(p2_time[j],p2_nbof[j]);
+      h1_pin2_ntimetrgbof->Fill(p2_ntimetrgbof[j]/10);
+      h2_pin2_ntimetrgbof->Fill(p2_time[j],p2_ntimetrgbof[j]);
+      prof_pin2_extemp->Fill(p2_time[j],p2_extemp[j]);
+      prof_pin2_boardtemp->Fill(p2_time[j],p2_boardtemp[j]);
+      prof_pin2_csptemp->Fill(p2_time[j],p2_csptemp[j]);
+      prof_pin2_vbias->Fill(p2_time[j],p2_vbias[j]*(-30.)/p2_vbias_mean);
+      h_pin2_adcval->Fill(p2_adcval[j]);
+    }
   }
 
   for (int b = 1; b <= max_p1_time/nbins; ++b) {
-    
+
     if (prof_pin1_adcval->GetBinContent(b)!=0) {
-      
+
       h1_pin1Dpin2_adcval->SetBinContent(b, prof_pin1_adcval->GetBinContent(b)/prof_pin2_adcval->GetBinContent(b));
-      
+
       h1_pin1Dpin2_adcval->SetBinError(b,
         sqrt(pow(prof_pin1_adcval->GetBinError(b)/prof_pin1_adcval->GetBinContent(b), 2)+
 	     pow(prof_pin2_adcval->GetBinError(b)/prof_pin2_adcval->GetBinContent(b), 2))*			               			      (prof_pin1_adcval->GetBinContent(b)/prof_pin2_adcval->GetBinContent(b)));
 
     } else {
-      
+
       h1_pin1Dpin2_adcval->SetBinContent(b, 0);
       h1_pin1Dpin2_adcval->SetBinError(b, 0);
-      
+
     }
   }
 
@@ -330,7 +323,7 @@ void NaDaqAna::finalize(string const &filename) {
   OutF->cd();
   gROOT->GetList()->Write();
   OutF->Close();
-  cout << "written file " << filename << endl;  
+  cout << "written file " << filename << endl;
   delete OutF;
   useDelete();
 
@@ -349,10 +342,10 @@ void NaDaqAna::useDelete() {
   delete h2_pin1_nbof;
   delete h2_pin2_nbof;
   delete h2_pin1Vpin2_adcval;
-  
+
   delete h1_pin1Dpin2_adcval;
 
-  // delete h1_pmt_ntimetrgbof;
+  delete h1_pmt_ntimetrgbof;
   delete h1_pin1_ntimetrgbof;
   delete h1_pin2_ntimetrgbof;
 
--- NapoliDaq/analyzer/NaDaqAna.h
+++ NapoliDaq/analyzer/NaDaqAna.h
@@ -23,7 +23,9 @@ class NaDaqAna : public EventCollection {
 
 private:
   Long64_t nentries;
-  Int_t iter;
+  Int_t pmt_iter;
+  Int_t p1_iter;
+  Int_t p2_iter;
 
   Double_t *pmt_Fired;
   Double_t *p1_Fired;
@@ -103,6 +105,6 @@ private:
   TH1D *h_pin2_adcval;
 
   TH1D *h1_pin1Dpin2_adcval;
-   
+  
 };
 #endif
